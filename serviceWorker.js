ac1590a0f02d909fbda461c014d9a2851c7de09e
64Terabytes <jamin.pepper15@gmail.com>
2021-07-27 09:54:24 +1000

    Changed some names

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticSCACAPP = "scac-student-app";
 const assets = [
   "/",
   "/index.html",
@@ -8,7 +8,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticSCACAPP).then(cache => {
       cache.addAll(assets);
     })
   );

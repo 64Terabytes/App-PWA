acedf1798369e7786c9215d6f0c49705e3eeaa6c
64Terabytes <jamin.pepper15@gmail.com>
2021-07-29 11:36:45 +1000

    added semi functioning code

--- js/app.js
+++ js/app.js
@@ -6,3 +6,37 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
+
+function ChangeTimeTableA() {
+  var TableYear;
+  if (document.getElementById("year-level").value == "year7") {
+    console.log("Opening Year 7 A")
+    document.getElementById("TimeTableImage").src = "images/timetable/year7A.png"
+  }
+  else if (document.getElementById("year-level").value == "year8") {
+    console.log("Opening Year 8 A")
+  }
+  else if (document.getElementById("year-level").value == "year9") {
+    console.log("Opening Year 9 A")
+  }
+  else if (document.getElementById("year-level").value == "year10") {
+    console.log("Opening Year 10 A")
+  }
+}
+
+function ChangeTimeTableB() {
+  var TableYear;
+  if (document.getElementById("year-level").value == "year7") {
+    console.log("Opening Year 7 B")
+    document.getElementById("TimeTableImage").src = "images/timetable/year7B.png"
+  }
+  else if (document.getElementById("year-level").value == "year8") {
+    console.log("Opening Year 8 B")
+  }
+  else if (document.getElementById("year-level").value == "year9") {
+    console.log("Opening Year 9 B")
+  }
+  else if (document.getElementById("year-level").value == "year10") {
+    console.log("Opening Year 10 B")
+  }
+}

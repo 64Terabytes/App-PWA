ef61304c864e72c58fbf4090ddbce6b688b4bb21
64Terabytes <jamin.pepper15@gmail.com>
2021-07-30 14:16:47 +1000

    Improved optimisation

--- js/app.js
+++ js/app.js
@@ -7,36 +7,9 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-function ChangeTimeTableA() {
+function ChangeTimeTable(Week) {
   var TableYear;
-  if (document.getElementById("year-level").value == "year7") {
-    console.log("Opening Year 7 A")
-    document.getElementById("TimeTableImage").src = "images/timetable/year7A.png"
-  }
-  else if (document.getElementById("year-level").value == "year8") {
-    console.log("Opening Year 8 A")
-  }
-  else if (document.getElementById("year-level").value == "year9") {
-    console.log("Opening Year 9 A")
-  }
-  else if (document.getElementById("year-level").value == "year10") {
-    console.log("Opening Year 10 A")
-  }
-}
-
-function ChangeTimeTableB() {
-  var TableYear;
-  if (document.getElementById("year-level").value == "year7") {
-    console.log("Opening Year 7 B")
-    document.getElementById("TimeTableImage").src = "images/timetable/year7B.png"
-  }
-  else if (document.getElementById("year-level").value == "year8") {
-    console.log("Opening Year 8 B")
-  }
-  else if (document.getElementById("year-level").value == "year9") {
-    console.log("Opening Year 9 B")
-  }
-  else if (document.getElementById("year-level").value == "year10") {
-    console.log("Opening Year 10 B")
-  }
+  var SelectedYearLevel = document.getElementById("year-level").value
+  console.log("Opening Year " + SelectedYearLevel + Week);
+  document.getElementById("TimeTableImage").src = "images/timetable/year" + SelectedYearLevel + Week + ".png";
 }
